94a0fdbab04117190e07ac398b51f7c856a422c2
Sergey <94781301+SergueiMoscow@users.noreply.github.com>
2023-07-04 01:46:04 +0300

    add concept for rent payment

--- rent/views.py
+++ rent/views.py
@@ -43,7 +43,7 @@ def payments(request):
             payment_obj.total = form.cleaned_data['total']
             payment_obj.bank_account = form.cleaned_data['bank_account']
             payment_obj.type = 'Alq'
-            payment_obj.concept = 'Аренда {form.cleaned_data["room"]}'
+            payment_obj.concept = f'Аренда {form.cleaned_data["room"]}'
             payment_obj.book_account = 'Приход'
             payment_obj.contract = Contract.get_active_contract_by_room(form.cleaned_data['room'])
             payment_obj.user = user
