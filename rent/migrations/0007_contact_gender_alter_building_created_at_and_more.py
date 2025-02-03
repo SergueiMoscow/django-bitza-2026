ac2071a8194603c970eaa2ed3b6a2a195999cef2
sergey <sbitza@gmail.com>
2025-02-03 23:30:00 +0300

    add generate contracts pdf

--- rent/migrations/0007_contact_gender_alter_building_created_at_and_more.py
+++ rent/migrations/0007_contact_gender_alter_building_created_at_and_more.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='contact',
             name='gender',
-            field=models.CharField(choices=[('F', 'Женский'), ('M', 'Мужской')], default='M', help_text='Пол: M - Мужской, F - Женский', max_length=1, verbose_name='Пол'),
+            field=models.CharField(choices=[('F', 'Женский'), ('M', 'Мужской')], null=True, default='M', help_text='Пол: M - Мужской, F - Женский', max_length=1, verbose_name='Пол'),
         ),
         migrations.AlterField(
             model_name='building',
